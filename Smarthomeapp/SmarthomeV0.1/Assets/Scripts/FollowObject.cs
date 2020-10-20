612370c441cd9e2dd63b2e855ffb9491d1a0d3d3
UgniusBrO <ugnius.mockus@gmail.com>
2020-10-20 08:51:44 +0300

    added light sensor
    added light sensor

---
+++ Smarthomeapp/SmarthomeV0.1/Assets/Scripts/FollowObject.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class FollowObject : MonoBehaviour
+{
+    public Transform target;
+    public Vector3 offset = new Vector3(0.0f, 0.0f, 0.0f);
+    private void LateUpdate()
+    {
+        transform.position = target.position + offset;
+    }
+}
---
+++ Smarthomeapp/SmarthomeV0.1/Assets/Scripts/LightCheck.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightCheck : MonoBehaviour
+{
+    public RenderTexture LightCheckTexture;
+    public float LightLevel;
+    public int Light;
+    // Update is called once per frame
+    void Update()
+    {
+        RenderTexture tmpTexture = RenderTexture.GetTemporary(LightCheckTexture.width, LightCheckTexture.height, 0, RenderTextureFormat.Default, RenderTextureReadWrite.Linear);
+        Graphics.Blit(LightCheckTexture, tmpTexture);
+        RenderTexture previous = RenderTexture.active;
+        RenderTexture.active = tmpTexture;
+
+        Texture2D temp2dTexture = new Texture2D(LightCheckTexture.width, LightCheckTexture.height);
+        temp2dTexture.ReadPixels(new Rect(0, 0, tmpTexture.width, tmpTexture.height), 0, 0);
+        temp2dTexture.Apply();
+
+        RenderTexture.active = previous;
+        //RenderTexture.RealeaseTemporary(tmpTexture);
+
+        Color32[] colors = temp2dTexture.GetPixels32();
+
+        LightLevel = 0;
+        for(int i = 0; i < colors.Length; i++)
+        {
+            LightLevel += (0.2126f * colors[i].r) + (0.7152f * colors[i].g) + (0.0722f + colors[i].b);
+        }
+        Debug.Log(LightLevel);
+    }
+}
